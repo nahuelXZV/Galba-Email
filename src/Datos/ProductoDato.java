ef95f53203150f4bd810402e7de4b239f5e2a813
DAVV422 <diegoalberto42216@gmail.com>
2023-11-11 09:28:44 -0400

    Create ProductoDato

---
+++ src/Datos/ProductoDato.java
@@ -0,0 +1,187 @@
+package Datos;
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.LinkedList;
+
+import Servicios.ConexionDB;
+
+public class ProductoDato {
+    private final ConexionDB conexion;
+
+    private int id;
+    private String nombre;
+    private String imagen;
+    private String tamaño;
+    private float precio;
+    private int cantidad;
+    private String descripcion;
+    private String categoria;
+
+    public ProductoDato() {
+        conexion = new ConexionDB();
+    }
+
+    public ProductoDato(int id, String nombre, String imagen, String tamaño, float precio, int cantidad,
+            String descripcion, String categoria) {
+        conexion = new ConexionDB();
+        this.id = id;
+        this.nombre = nombre;
+        this.imagen = imagen;
+        this.tamaño = tamaño;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+    }
+
+    // Funciones
+    public boolean createProducto() {
+        String sql = "INSERT INTO producto (nombre , imagen, tamaño, precio, cantidad, descripcion, categoria) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try (Connection con = conexion.connect(); PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, nombre);
+            ps.setString(2, imagen);
+            ps.setString(3, tamaño);
+            ps.setFloat(4, precio);
+            ps.setInt(5, cantidad);
+            ps.setString(6, descripcion);
+            ps.setString(7, categoria);
+            int rowsAffected = ps.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean updateProducto() {
+        String sql = "UPDATE usuario SET nombre = ?, imagen = ?, tamaño = ?, precio = ?, cantidad = ?, descripcion = ?, categoria = ? WHERE id = ?";
+        try (Connection con = conexion.connect(); PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, nombre);
+            ps.setString(2, imagen);
+            ps.setString(3, tamaño);
+            ps.setFloat(4, precio);
+            ps.setInt(5, cantidad);
+            ps.setString(6, descripcion);
+            ps.setString(7, categoria);
+            ps.setInt(8, id);
+            int rowsAffected = ps.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean delete(int id) {
+        String sql = "DELETE FROM producto WHERE id = ?";
+        try (Connection con = conexion.connect(); PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            int rowsAffected = ps.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean exist(int id) {
+        String sql = "SELECT * FROM producto WHERE id = ?";
+        try (Connection con = conexion.connect(); PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            int rowsAffected = ps.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean emailExist(String correo) {
+        String sql = "SELECT * FROM usuario WHERE correo = ?";
+        try (Connection con = conexion.connect(); PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, correo);
+            int rowsAffected = ps.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean validateRol(String correo, String rol, String atribute) {
+        String sql = "SELECT * FROM usuario WHERE ? = ? AND correo = ?";
+        try (Connection con = conexion.connect(); PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, atribute);
+            ps.setString(2, rol);
+            ps.setString(3, correo);
+            int rowsAffected = ps.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public String getAll(LinkedList<String> params) {
+        String tabla = "";
+        try {
+            Statement consulta;
+            ResultSet resultado = null;
+            tabla = "<h1>Lista de productos</h1>\n"
+                    + "<table style=\"border-collapse: collapse; width: 100%; border: 1px solid black;\">\n"
+                    + "\n"
+                    + "  <tr>\n"
+                    + "\n"
+                    + "    <th style = \"text-align: left; padding: 8px; background-color: #3c4f76; color: white; border: 1px solid black;\">ID</th>\n"
+                    + "\n"
+                    + "    <th style = \"text-align: left; padding: 8px; background-color: #3c4f76; color: white; border: 1px solid black;\">NOMBRE</th>\n"
+                    + "\n"
+                    + "    <th style = \"text-align: left; padding: 8px; background-color: #3c4f76; color: white; border: 1px solid black;\">IMAGEN</th>\n"
+                    + "\n"
+                    + "    <th style = \"text-align: left; padding: 8px; background-color: #3c4f76; color: white; border: 1px solid black;\">TAMAÑO</th>\n"
+                    + "\n"
+                    + "    <th style = \"text-align: left; padding: 8px; background-color: #3c4f76; color: white; border: 1px solid black;\">PRECIO</th>\n"
+                    + "\n"
+                    + "    <th style = \"text-align: left; padding: 8px; background-color: #3c4f76; color: white; border: 1px solid black;\">CANTIDAD</th>\n"
+                    + "\n"
+                    + "    <th style = \"text-align: left; padding: 8px; background-color: #3c4f76; color: white; border: 1px solid black;\">DESCRIPCION</th>\n"
+                    + "\n"
+                    + "    <th style = \"text-align: left; padding: 8px; background-color: #3c4f76; color: white; border: 1px solid black;\">CATEGORIA</th>\n"
+                    + "\n";
+            String query = "";
+            if (params.size() == 0)
+                query = "SELECT id, nombre, imagen, tamaño, precio, cantidad, descripcion, categoria FROM producto";
+            else
+                query = "SELECT id, nombre, imagen, tamaño, precio, cantidad, descripcion, categoria FROM producto WHERE "
+                        + params.get(0) + " ILIKE '%" + params.get(1) + "%'";
+            Connection con = conexion.connect();
+            consulta = (Statement) con.createStatement();
+            resultado = ((java.sql.Statement) consulta).executeQuery(query);
+            ResultSetMetaData rsmd = resultado.getMetaData();
+            int cantidadColumnas = rsmd.getColumnCount();
+            while (resultado.next()) {
+                tabla = tabla + "  <tr>\n" + "\n";
+                for (int i = 0; i < cantidadColumnas; i++) {
+                    tabla = tabla
+                            + "    <td style = \"text-align: left; padding: 8px; border: 1px solid black;\">"
+                            + resultado.getString(i + 1) + "</td>\n"
+                            + "\n";
+                }
+                tabla = tabla + "  </tr>\n" + "\n";
+            }
+            tabla = tabla + "\n" + "</table>";
+            ((Connection) consulta).close();
+            con.close();
+        } catch (SQLException e) {
+            tabla = "No se pudieron listar los datos.";
+            System.out.println(e.getMessage());
+        }
+        return tabla;
+    }
+
+}
